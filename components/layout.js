c367e0c2837aabf4ff7c3b02a5faa7c24f584203
Vivien Ogoun <vivienogoun1@gmail.com>
2023-06-19 09:28:49 +0100

    Change site title

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
 const name = 'Vivien Ogoun';
-export const siteTitle = 'Next.js Sample Website';
+export const siteTitle = 'Vivien Ogoun';
 
 export default function Layout({ children, home }) {
   return (
